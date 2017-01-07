c279229513f65a9bd15c3a65bdafb8bee2392e10
hawnzug <hawnzug@gmail.com>
2017-01-07 17:58:39 +0800

    Mark 5: Better handling of arithmetic

--- src/gmachine.rs
+++ src/gmachine.rs
@@ -49,6 +49,12 @@ enum Node {
     NInd(Addr),
 }
 
+#[derive(Debug, Clone)]
+enum Context {
+    Strict,
+    Lazy,
+}
+
 pub fn compile(prog: Program) -> GmState {
     use self::Instruction::{Eval, PushGlobal};
     let init_code = vec![Eval, PushGlobal(String::from("main"))];
@@ -107,9 +113,71 @@ fn compile_sc(scdef: ScDef) -> (Name, usize, GmCode) {
 fn compile_r(expr: Expr, env: GmEnvironment) -> GmCode {
     use self::Instruction::{Unwind, Pop, Update};
     let mut init = vec![Unwind, Pop(env.len()), Update(env.len())];
-    let mut compiled = compile_c(expr, env);
+    let mut compiled = compile_e(expr, env);
     init.append(&mut compiled);
     init
+}
+
+fn compile_e(expr: Expr, env: GmEnvironment) -> GmCode {
+    use self::Instruction::*;
+    match expr {
+        Expr::ENum(i) => vec![Pushint(i)],
+        Expr::ELet(defs, box_expr) => compile_let(defs, *box_expr, env, Context::Strict),
+        Expr::ELetrec(defs, box_expr) => compile_letrec(defs, *box_expr, env, Context::Strict),
+        _ => {
+            let expr = if let Expr::EAp(e1, e2) = expr {
+                let temp = *e1;
+                let e1 = match temp {
+                    Expr::EVar(name) => {
+                        if name == "negate" {
+                            let mut init = vec![Neg];
+                            let mut compiled = compile_e(*e2, env);
+                            init.append(&mut compiled);
+                            return init;
+                        } else {
+                            Box::new(Expr::EVar(name))
+                        }
+                    }
+                    Expr::EAp(e3, e4) => {
+                        let e3 = match *e3 {
+                            Expr::EVar(name) => {
+                                let mut init = match name.as_ref() {
+                                    "+" => vec![Add],
+                                    "-" => vec![Sub],
+                                    "*" => vec![Mul],
+                                    "/" => vec![Div],
+                                    _ => vec![],
+                                };
+                                if init.is_empty() {
+                                    Box::new(Expr::EVar(name))
+                                } else {
+                                    let new_env = env.clone()
+                                        .into_iter()
+                                        .map(|(name, i)| (name, i + 1))
+                                        .collect();
+                                    let mut compiled = compile_e(*e4, new_env);
+                                    init.append(&mut compiled);
+                                    let mut compiled = compile_e(*e2, env);
+                                    init.append(&mut compiled);
+                                    return init;
+                                }
+                            }
+                            _ => e3,
+                        };
+                        Box::new(Expr::EAp(e3, e4))
+                    }
+                    _ => Box::new(temp),
+                };
+                Expr::EAp(e1, e2)
+            } else {
+                expr
+            };
+            let mut code = vec![Eval];
+            let mut left = compile_c(expr, env);
+            code.append(&mut left);
+            code
+        }
+    }
 }
 
 fn compile_c(expr: Expr, env: GmEnvironment) -> GmCode {
@@ -132,13 +200,13 @@ fn compile_c(expr: Expr, env: GmEnvironment) -> GmCode {
             init.append(&mut compiled2);
             init
         }
-        Expr::ELet(defs, box_expr) => compile_let(defs, *box_expr, env),
-        Expr::ELetrec(defs, box_expr) => compile_letrec(defs, *box_expr, env),
+        Expr::ELet(defs, box_expr) => compile_let(defs, *box_expr, env, Context::Lazy),
+        Expr::ELetrec(defs, box_expr) => compile_letrec(defs, *box_expr, env, Context::Lazy),
         _ => unreachable!(),
     }
 }
 
-fn compile_let(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment) -> GmCode {
+fn compile_let(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment, context: Context) -> GmCode {
     use self::Instruction::Slide;
     let len = defs.len();
     let mut code = vec![Slide(len)];
@@ -149,7 +217,10 @@ fn compile_let(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment) -> GmCode {
     for (i, pair) in defs.iter().enumerate() {
         new_env.insert(pair.0.clone(), len - i - 1);
     }
-    let mut code_expr = compile_c(expr, new_env);
+    let mut code_expr = match context {
+        Context::Strict => compile_e(expr, new_env),
+        Context::Lazy => compile_c(expr, new_env),
+    };
     code.append(&mut code_expr);
     for (n, (_, e)) in defs.into_iter().enumerate().rev() {
         let new_env = env.clone().into_iter().map(|(name, i)| (name, i + n)).collect();
@@ -158,7 +229,7 @@ fn compile_let(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment) -> GmCode {
     code
 }
 
-fn compile_letrec(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment) -> GmCode {
+fn compile_letrec(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment, context: Context) -> GmCode {
     use self::Instruction::{Slide, Update, Alloc};
     let len = defs.len();
     let mut code = vec![Slide(len)];
@@ -169,7 +240,10 @@ fn compile_letrec(defs: Vec<LetEq>, expr: Expr, env: GmEnvironment) -> GmCode {
         new_env.insert(pair.0.clone(), len - i - 1);
     }
     let new_env_clone = new_env.clone();
-    let mut code_expr = compile_c(expr, new_env);
+    let mut code_expr = match context {
+        Context::Strict => compile_e(expr, new_env),
+        Context::Lazy => compile_c(expr, new_env),
+    };
     code.append(&mut code_expr);
     for (n, (_, e)) in defs.into_iter().enumerate().rev() {
         let new_env = new_env_clone.clone();
@@ -275,11 +349,20 @@ impl GmState {
                 self.stack.push(a);
             }
             Node::NGlobal(n, c) => {
-                self.code = c;
                 let len = self.stack.len();
-                for i in 0..n {
-                    let j = len - 1 - i;
-                    self.stack[j] = get_arg(self.heap.lookup(self.stack[j - 1]).unwrap());
+                if len - 1 < n {
+                    if let Some((old_code, old_stack)) = self.dump.pop() {
+                        self.code = old_code;
+                        let addr = self.stack[0];
+                        self.stack = old_stack;
+                        self.stack.push(addr);
+                    }
+                } else {
+                    self.code = c;
+                    for i in 0..n {
+                        let j = len - 1 - i;
+                        self.stack[j] = get_arg(self.heap.lookup(self.stack[j - 1]).unwrap());
+                    }
                 }
             }
         }
